f879b472e6fb8f585c7089926ab7d2b47050be11
Joshua Jung <wbqdinno@gmail.com>
2016-05-14 02:19:43 +0900

    add theme

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,4 +34,33 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  .config(function ($mdThemingProvider) {
+    $mdThemingProvider.definePalette('blue-ngo', {
+      '50': 'E3F2FD',
+      '100': 'BBDEFB',
+      '200': '90CAF9',
+      '300': '64B5F6',
+      '400': '42A5F5',
+      '500': '4F8FCC',
+      '600': '1E88E5',
+      '700': '1976D2',
+      '800': '193C6C',
+      '900': '0D47A1',
+      'A100': '82B1FF',
+      'A200': '448AFF',
+      'A400': '2979FF',
+      'A700': '2962FF',
+      'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                          // on this palette should be dark or light
+      'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+        '200', '300', '400', 'A100'],
+      'contrastLightColors': undefined    // could also specify this if default was 'dark'
+    });
+    $mdThemingProvider.theme('ngo')
+      .primaryPalette('blue-ngo')
+      .accentPalette('amber')
+      .warnPalette('red')
+      .backgroundPalette('grey');
+  })
+;
