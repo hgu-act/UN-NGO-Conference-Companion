f9c4666d77fc7364b99b4e086be19f66d80a2a47
Joshua Jung <wbqdinno@gmail.com>
2016-05-14 02:07:35 +0900

    add AppCtrl

---
+++ app/scripts/controllers/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngoConferenceCompanionApp.controller:AppCtrl
+ * @description
+ * # AppCtrl
+ * Controller of the ngoConferenceCompanionApp
+ */
+angular.module('ngoConferenceCompanionApp')
+  .controller('AppCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
