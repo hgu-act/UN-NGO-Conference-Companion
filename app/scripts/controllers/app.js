dd44d7775807045bd8e881c21267777eb83dd632
Joshua Jung <wbqdinno@gmail.com>
2016-05-14 03:50:12 +0900

    make sideNavMenu close on route

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -8,10 +8,30 @@
  * Controller of the ngoConferenceCompanionApp
  */
 angular.module('ngoConferenceCompanionApp')
-  .controller('AppCtrl', function ($scope, $mdSidenav, sideNavMenu) {
+  .controller('AppCtrl', function ($scope, $mdSidenav, $location, sideNavMenu) {
     $scope.menu = sideNavMenu;
+    $scope.toolbarTitle = 'Home';
 
     $scope.toggleSidenav = function (menuId) {
       $mdSidenav(menuId).toggle();
     };
+
+    $scope.routeAndClose = function (menuId, menuItem) {
+      $mdSidenav(menuId).close();
+      $scope.toolbarTitle = menuItem.title;
+      $location.path(menuItem.url);
+    };
+
+    $scope.switchToolbarTitle = function () {
+      switch ($location.path()) {
+        case 'timeline':
+          $scope.toolbarTitle = 'Timeline';
+          break;
+        case 'schedule':
+          $scope.toolbarTitle = 'Schedule';
+          break;
+        default:
+          break;
+      }
+    }
   });
