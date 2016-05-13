c6021bc36e0b92198d524b794c65d9d9ff92c36d
Joshua Jung <wbqdinno@gmail.com>
2016-05-14 03:50:44 +0900

    add sideNavMenu items

--- app/scripts/services/sidenavmenu.js
+++ app/scripts/services/sidenavmenu.js
@@ -10,9 +10,43 @@
 angular.module('ngoConferenceCompanionApp')
   .value('sideNavMenu', [
     {
-      url: '#timeline',
+      url: 'timeline',
       title: 'Timeline',
-      icon: 'timeline',
-      
+      icon: 'timeline'
+    },
+    {
+      url: 'schedule',
+      title: 'Schedule',
+      icon: 'list'
+    },
+    {
+      url: '#transportation',
+      title: 'Transportation',
+      icon: 'directions_bus'
+    },
+    {
+      url: '#accommodation',
+      title: 'Accommodation',
+      icon: 'hotel'
+    },
+    {
+      url: '#tourism',
+      title: 'Tourism',
+      icon: 'map'
+    },
+    {
+      url: '#floor-plans',
+      title: 'Floor Plans',
+      icon: 'layers'
+    },
+    {
+      url: '#visit-korea',
+      title: 'Visit Korea ™',
+      icon: 'link'
+    },
+    {
+      url: '#1330-korea-travel-hotline',
+      title: '1330 Korea Travel Hotline',
+      icon: 'link'
     }
   ]);
