4e1fc469806270479a814bedd2c1348f277f6633
Joshua Jung <wbqdinno@gmail.com>
2016-05-23 08:50:24 +0900

    add cache feature to notice query

--- app/scripts/services/resource.js
+++ app/scripts/services/resource.js
@@ -9,7 +9,10 @@
  */
 angular.module('ngoConferenceCompanionApp')
   .factory('resource', function ($cacheFactory, $resource) {
-    var notice = $resource('http://52.78.1.73:3000/notice/:id', {}, {get: {cache: true, method: 'get'}});
+    var notice = $resource('http://52.78.1.73:3000/notice/:id', {}, {
+      get: {cache: true, method: 'get'},
+      query: {cache: true, method: 'get', isArray:true}
+    });
     var service = {
       notice: notice
     };
