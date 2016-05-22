c090ee58b5c29d402962be5a18db1e9d46a610cd
Joshua Jung <wbqdinno@gmail.com>
2016-05-23 07:18:02 +0900

    auto fix: wiedep

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-material/angular-material.js',
-      'bower_components/angular-timeago/src/timeAgo.js',
+      'bower_components/angular-timeago/dist/angular-timeago.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
