f5b44dbc305d5c08b1a404f42561f57ab85ebe39
Joshua Jung <wbqdinno@gmail.com>
2016-05-23 08:49:49 +0900

    modify notice view

--- app/scripts/controllers/notice.js
+++ app/scripts/controllers/notice.js
@@ -11,34 +11,6 @@ angular.module('ngoConferenceCompanionApp')
   .controller('NoticeCtrl', function (resource, $log, timeAgo) {
     var noticeVM = this;
 
-    noticeVM.toolbarTitle = 'Home';
-    noticeVM.cardImgTextStyle = {
-      'color': 'white',
-      'padding': '16px'
-    };
-    noticeVM.currentEvent = {
-      title: 'Opening Session',
-      type: 'opening',
-      startDate: '2016-05-31T10:00',
-      endDate: '2016-05-31T12:00',
-      duration: '120\'',
-      place: '300A+300B+300C',
-      backgroundStyle: {
-        'background': 'url("https://s3.ap-northeast-2.amazonaws.com/ngo-app/opening-session.png") center / cover',
-        'height': '176px'
-      }
-    };
-    noticeVM.nextEvent = {
-      title: 'Lunch',
-      type: 'lunch',
-      startDate: '2016-05-31T12:00',
-      endDate: '2016-05-31T13:30',
-      duration: '90\'',
-      place: 'Around HICO',
-      backgroundStyle: {
-        'background': 'url("https://s3.ap-northeast-2.amazonaws.com/ngo-app/lunch.png") center / cover',
-        'height': '176px'
-      }
-    };
+    noticeVM.toolbarTitle = 'Notice';
     noticeVM.list = resource.notice.query();
   });
