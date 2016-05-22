5f7383587f827f4239dbaaaf91c13df68334f4aa
Joshua Jung <wbqdinno@gmail.com>
2016-05-23 07:18:22 +0900

    fix url of sideNav

--- app/scripts/services/sidenavmenu.js
+++ app/scripts/services/sidenavmenu.js
@@ -10,32 +10,37 @@
 angular.module('ngoConferenceCompanionApp')
   .value('sideNavMenu', [
     {
+      url: 'home',
+      title: 'Home',
+      icon: 'home'
+    },
+    {
       url: 'timetable',
       title: 'Timetable',
       icon: 'list'
     },
     {
-      url: '#concept-note',
+      url: 'concept-note',
       title: 'Concept Note',
       icon: 'event_note'
     },
     {
-      url: '#roundtable',
+      url: 'roundtable',
       title: 'Roundtable',
       icon: 'event_seat'
     },
     {
-      url: '#workshop',
+      url: 'workshop',
       title: 'Workshop',
       icon: 'build'
     },
     {
-      url: '#exhibits',
+      url: 'exhibits',
       title: 'Exhibits',
       icon: 'local_activity'
     },
     {
-      url: '#visit-korea',
+      url: 'visit-korea',
       title: 'Visit Korea ™',
       icon: 'map'
     }
