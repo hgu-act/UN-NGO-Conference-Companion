c5720b47954a6421ba99a14fd7a69beff7c2236e
Joshua Jung <wbqdinno@gmail.com>
2016-05-23 02:43:43 +0900

    update notice page to display text in picture

--- app/scripts/controllers/notice.js
+++ app/scripts/controllers/notice.js
@@ -9,9 +9,36 @@
  */
 angular.module('ngoConferenceCompanionApp')
   .controller('NoticeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var noticeVM = this;
+    noticeVM.toolbarTitle = 'Home';
+    noticeVM.cardImgTextStyle = {
+      "color": "white",
+      "padding": "16px"
+    };
+
+    noticeVM.currentEvent = {
+      title: 'Opening Session',
+      type: 'opening',
+      startDate: '2016-05-31T10:00',
+      endDate: '2016-05-31T12:00',
+      duration: '120\'',
+      place: '300A+300B+300C',
+      backgroundStyle: {
+        "background": "url('https://s3.ap-northeast-2.amazonaws.com/ngo-app/opening-session.png') center / cover",
+        "height": "176px"
+      }
+    };
+
+    noticeVM.nextEvent = {
+      title: 'Lunch',
+      type: 'lunch',
+      startDate: '2016-05-31T12:00',
+      endDate: '2016-05-31T13:30',
+      duration: '90\'',
+      place: 'Around HICO',
+      backgroundStyle: {
+        "background": "url('https://s3.ap-northeast-2.amazonaws.com/ngo-app/lunch.png') center / cover",
+        "height": "176px"
+      }
+    };
   });
