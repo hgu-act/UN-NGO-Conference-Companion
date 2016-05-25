7dfacd305c9cc48f9c9a742786208efbcc59e356
Joshua Jung <wbqdinno@gmail.com>
2016-05-25 22:30:11 +0900

    Gruntfile.js: add let-it-go task

--- Gruntfile.js
+++ Gruntfile.js
@@ -471,6 +471,10 @@ module.exports = function (grunt) {
     ]);
   });
 
+  grunt.registerTask('letitgo', [
+    'connect:dist:keepalive'
+  ]);
+
   grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
