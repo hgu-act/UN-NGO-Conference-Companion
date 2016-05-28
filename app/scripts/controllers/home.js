ca145a8ffe2c09c6987e6842c5e8365f338b2a85
Joshua Jung <wbqdinno@gmail.com>
2016-05-29 00:48:04 +0900

    add event duration

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -16,8 +16,14 @@ angular.module('ngoConferenceCompanionApp')
       'color': 'white',
       'padding': '16px'
     };
-    homeVM.currentEvent = timetableValue[0].schedules[0]; 
+    homeVM.currentEvent = timetableValue[0].schedules[0];
     homeVM.nextEvent = timetableValue[0].schedules[1];
     homeVM.list = resource.notice.query();
     homeVM.mapper = mapper;
+
+    var currentDiff = Date.parse(homeVM.currentEvent.endDate) - Date.parse(homeVM.currentEvent.startDate);
+    var nextDiff = Date.parse(homeVM.nextEvent.endDate) - Date.parse(homeVM.nextEvent.startDate);
+
+    homeVM.currentDuration = currentDiff / 1000 / 60;
+    homeVM.nextDuration = nextDiff / 1000 / 60;
   });
