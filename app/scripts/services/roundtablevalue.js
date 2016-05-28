a8946f15be7348879119e0b81489f2b2b4a425a0
Joshua Jung <wbqdinno@gmail.com>
2016-05-29 05:28:50 +0900

    implement roundtable view

--- app/scripts/services/roundtablevalue.js
+++ app/scripts/services/roundtablevalue.js
@@ -12,36 +12,131 @@ angular.module('ngoConferenceCompanionApp')
     {
       title: 'Roundtable 1',
       id: 1,
+      topic: 'The Right to Accessible, Safe and Inclusive Learning Spaces',
+      detail: '<p>Education must leave no one behind, and be accessible to all throughout life through quality learning opportunities. Too many of the world’s students and potential learners are explicitly denied, or subtly pushed away from, educational and training opportunities for reasons including their gender, gender identity, race, ethnicity, language, location, religion, sexual orientation, age, ability (or disability), or poverty.</p>' +
+      '<p>A combination of factors including bullying, discrimination, micro aggressions, gender-based violence and lack of facilities such as classrooms, teaching and learning materials, water and sanitation, and lack of financing deny education and learning opportunities to many students, including 775 million adults, a disproportionate number of them women, who lack minimum literacy skills. This session will explore how the education community can ensure that all individuals have the opportunity to learn and grow to their maximum potential as stated in Sustainable Development Goal 4, and why this is crucial to achieving all of the SDGs.</p>' +
+      '<p>The roundtable will recommend concrete formal actions, such as through national education plans and international frameworks for policies and cooperation to improve education, and offer informal approaches including training, mentoring, social mobilization, building public awareness, and public and private partnerships to narrow access and quality gaps in education.</p>',
       files: [
-        // {
-        //   url: '',
-        //   title: 'Roundtable 1 Example PDF'
-        // },
+        {
+          url: '',
+          icon: 'watch_later',
+          title: 'Available after Roundtable'
+        }
       ]
     },
     {
       title: 'Roundtable 2',
       id: 2,
+      topic: 'STEAM Should Power the SDGs',
+      detail: '<p>Expanding opportunities for scientific training is an essential component of economic development for all nations. Science, technology, engineering, art, and math, or “STEAM,” is an interdisciplinary learning model that offers new opportunities for creating sustainable models of production and consumption, public health, urban design and infrastructure, water distribution, and food and energy production.</p>' +
+      '<p>Despite these multiple benefits, STEAM faces critical, interlinked financing and access gaps. Science, technology, engineering and math are stubbornly difficult to access for many in developed countries, and in particular for girls. The technology gap, and financing for it, grows exponentially in developing countries and is particularly acute for marginalized groups. Art and design in their purest form also suffer from a lack of investment, leaving questions about the value governments and communities put on creative thinking as a driver for innovation and problem solving.</p>' +
+      '<p>This roundtable will explore practical investment, policy and advocacy strategies to expand access to science, technology, engineering, art and math education that “leaves no one behind” and proactively reaches out to girls and women. Best-practice examples of STEAM education and information provided to socially and culturally vulnerable people including girls, women and minorities will be presented, highlighting how innovation can help confront the most pressing current research challenges facing the SDGs. Examples of how art and design can be deployed to spark innovation in scientific and engineering contexts will also be offered.</p>' +
+      '<p>Advances in information and communication technology (ICT) that offer major new opportunities for the delivery of innovative STEAM curricula to schools and educational centers around the world, rich and poor, will be discussed.</p>',
       files: [
+        {
+          url: '',
+          icon: 'watch_later',
+          title: 'Available after Roundtable'
+        }
       ]
     },
     {
       title: 'Roundtable 3',
       id: 3,
+      topic: 'Children and Youth: Tomorrow’s Global Citizens Today',
+      detail: '<p>This conference acknowledges the centrality of SDG 4 as a driver for transformational change, and the primacy of educating children as the starting point for achieving all 17 SDGs. Enrollment in primary education in developing countries has reached 91 per cent, but 58 million children of primary school age, and 63 million children of lower secondary age remain out of school, with sub-Saharan Africa and conflict zones representing stubborn pockets of exclusion.</p>' +
+      '<p>This roundtable will aim to galvanize global commitment to ensure that all girls and boys get twelve years of free, publicly-funded formal quality education, nine of which should be compulsory, and to discuss effective strategies to invest in early childhood care and education (ECCE) programs and services. The session will focus on concrete ways to ensure quality education for children in armed conflict and post-conflict situations including effective measures to deal with trauma and other impediments, as well as peace education to help avoid conflict. Formal and informal education together with advocacy for global citizenship which puts a premium on building a culture of peace and social well-being for all will be explored. The session will also offer approaches to avoid breaks in education due to natural disasters and public health emergencies.</p>' +
+      '<p>The roundtable will also examine the disparity in the amounts that governments spend on military and education sectors, respectively, and highlight the economic and social benefits that higher budget allocations for primary and secondary education in particular promise.</p>',
       files: [
-        
+        {
+          url: '',
+          icon: 'watch_later',
+          title: 'Available after Roundtable'
+        }
       ]
     },
     {
       title: 'Roundtable 4',
       id: 4,
+      topic: 'Global Citizens as Stewards of the Planet: Energy, Environment and Climate Change',
+      detail: '<p>In today’s era of climate change, global pollution, natural resource depletion, and threats to biodiversity, societies are reassessing the value placed on the natural environment and exploring how formal and informal education, training and grassroots advocacy can strengthen humankind’s capacities to exist on this planet.</p>' +
+      '<p>This roundtable will explore why cultivating empathy and a scientific appreciation for the natural world, environmental justice, and responsibility towards future generations must be at the core of education for global citizenship. It will feature the voices and teachings of indigenous cultures to help identify the values and skill sets necessary for sustainable production and consumption that will protect both life below water and life on land. Indigenous teachings affirm reverence for “all our relations,” the kinship of all life. These and other traditional values and ecological knowledge systems will be juxtaposed with environmental science and new approaches to conserving natural wealth and capital to address a range of looming challenges, including climate change and population growth.</p>' +
+      '<p>In addition, this roundtable will address the pivotal role that increased access to sustainable energy will have in ensuring each person’s right to education, health, and a healthy environment.</p>',
       files: [
+        {
+          url: '',
+          icon: 'watch_later',
+          title: 'Available after Roundtable'
+        }
       ]
     },
     {
       title: 'Special Roundtable',
       id: 5,
+      topic: 'Local Development and Poverty Eradication for Global Citizenship',
+      subtitle: 'Sharing the Saemaul Undong experience from Korea and other developing countries as a case study in eradicating poverty and nation building in support of the Sustainable Development Goals',
+      detail: '<p>Korea has a unique experience of rising from the tribulations of colonization and war to build a democratic nation with a thriving economy, and a deep respect for human rights. Saemaul Undong originated from the Canaan Farmers School as an agriculture pioneer movement and the Korean government adopted it as a national poverty eradication campaign focusing on education for citizenship. Saemaul Undong emphasized:</p>' +
+      '<ol>' +
+      '<li>Poverty eradication</li>' +
+      '<li>Improvement of health services and child care</li>' +
+      '<li>Empowerment of local communities</li>' +
+      '<li>Revitalization of community leadership and intergenerational leadership</li>' +
+      '<li>Women\'s participation in the community</li>' +
+      '<li>Microfinancing for villages</li>' +
+      '</ol>' +
+      '<p>Gyeongsangbuk-do Province is hosting this year’s conference and is known as the place where the Saemaul Undong originated.</p>' +
+      '<p>The session will share how this civic movement contributed to economic development and improved human rights and brought awareness for environmental sustainability in the Korean context and how it could be applied around the world today.</p>',
       files: [
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Citizen+Education+for+Rural+Development_Edward+P.+Reed_English.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Citizen Education for Rural Development_Edward P. Reed_English'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Citizen+Education+for+Rural+Development_Edward+P.+Reed_Korean.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Citizen Education for Rural Development_Edward P. Reed_Korean'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Citizen+Education+for+Rural+Development_Edward+P.+Reed_ppt.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Citizen Education for Rural Development_Edward P. Reed_ppt'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Saemaul+Undong_a+viable+rural+development+model+for+Africa_Mamadou+Ndiaye_English.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Saemaul Undong_a viable rural development model for Africa_Mamadou Ndiaye_English'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Saemaul+Undong_a+viable+rural+development+model+for+Africa_Mamadou+Ndiaye_French.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Saemaul Undong_a viable rural development model for Africa_Mamadou Ndiaye_French'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Saemaul+Undong_a+viable+rural+development+model+for+Africa_Mamadou+Ndiaye_Korean.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Saemaul Undong_a viable rural development model for Africa_Mamadou Ndiaye_Korean'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/The+Role+of+Saemaul+Undong+in+Achieving+the+UN+Sustainable+Development+Goals_Hyun-Ki+Kim_English.pdf',
+          icon: 'picture_as_pdf',
+          title: 'The Role of Saemaul Undong in Achieving the UN Sustainable Development Goals_Hyun-Ki Kim_English'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/The+Role+of+Saemaul+Undong+in+Achieving+the+UN+Sustainable+Development+Goals_Hyun-Ki+Kim_Korean.pdf',
+          icon: 'picture_as_pdf',
+          title: 'The Role of Saemaul Undong in Achieving the UN Sustainable Development Goals_Hyun-Ki Kim_Korean'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Transformation+of+Leader+for+Sustainable+Rural+Development+and+Poverty+Eradication_Bumil+Kim_English.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Transformation of Leader for Sustainable Rural Development and Poverty Eradication_Bumil Kim_English'
+        },
+        {
+          url: 'https://s3.ap-northeast-2.amazonaws.com/ngo-app/roundtables/special-roundtable/Transformation+of+Leader+for+Sustainable+Rural+Development+and+Poverty+Eradication_Bumil+Kim_Korean.pdf',
+          icon: 'picture_as_pdf',
+          title: 'Transformation of Leader for Sustainable Rural Development and Poverty Eradication_Bumil Kim_Korean'
+        }
       ]
     }
   ]);
