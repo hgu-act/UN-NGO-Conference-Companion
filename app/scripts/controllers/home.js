d4462c0d38f520b0f5c5ae53c61a17bdaa50e36f
Joshua Jung <wbqdinno@gmail.com>
2016-05-30 13:26:20 +0900

    replace find method

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -13,54 +13,37 @@ angular.module('ngoConferenceCompanionApp')
     var events = timetableValue[0].schedules.concat(timetableValue[1].schedules, timetableValue[2].schedules);
     var today = new Date();
     var now = today.getTime();
+    homeVM.currentEvents = [];
 
     homeVM.toolbarTitle = 'Home';
     homeVM.cardImgTextStyle = {
       'color': 'white',
       'padding': '16px'
     };
-    homeVM.currentEvent = getCurrentEvent();
-    homeVM.currentEventTwo = getCurrentEventTwo(homeVM.currentEvent);
-    homeVM.currentEventThree = getCurrentEventThree(homeVM.currentEvent, homeVM.currentEventTwo);
-    homeVM.nextEvent = getNextEvent();
+    // homeVM.currentEvent = getCurrentEvent();
+    // homeVM.currentEventTwo = getCurrentEventTwo(homeVM.currentEvent);
+    // homeVM.currentEventThree = getCurrentEventThree(homeVM.currentEvent, homeVM.currentEventTwo);
+    homeVM.nextEvents = [];
     homeVM.list = resource.notice.query();
     homeVM.mapper = mapper;
 
-    homeVM.currentDuration = getDuration(homeVM.currentEvent);
-    homeVM.currentTwoDuration = getDuration(homeVM.currentEventTwo);
-    homeVM.currentThreeDuration = getDuration(homeVM.currentEventThree);
-    homeVM.nextDuration = getDuration(homeVM.nextEvent);
+    // homeVM.currentDuration = getDuration(homeVM.currentEvent);
+    // homeVM.currentTwoDuration = getDuration(homeVM.currentEventTwo);
+    // homeVM.currentThreeDuration = getDuration(homeVM.currentEventThree);
+    // homeVM.nextDuration = getDuration(homeVM.nextEvent);
 
-    function getCurrentEvent() {
-      return events.find(function (event) {
-        return now >= Date.parse(event.startDate) && now <= Date.parse(event.endDate);
-      });
-    }
+    angular.forEach(events, function (event) {
+      if (now >= Date.parse(event.startDate) && now <= Date.parse(event.endDate))
+        this.push(event)
+    }, homeVM.currentEvents);
 
-    function getCurrentEventTwo(eventOne) {
-      return events.find(function (event) {
-        return now >= Date.parse(event.startDate) && now <= Date.parse(event.endDate) && !isEqual(event, eventOne);
-      });
-    }
+    angular.forEach(events, function (event) {
+      if (now < Date.parse(event.startDate) && now < Date.parse(event.endDate))
+        this.push(event)
+    }, homeVM.nextEvents);
 
-    function getCurrentEventThree(eventOne, eventTwo) {
-      return events.find(function (event) {
-        return now >= Date.parse(event.startDate) && now <= Date.parse(event.endDate) && !isEqual(event, eventOne) && !isEqual(event, eventTwo);
-      });
-    }
-
-    function getNextEvent() {
-      return events.find(function (event) {
-        return now < Date.parse(event.startDate) && now < Date.parse(event.endDate);
-      });
-    }
-
-    function getDuration(event) {
-      if (event !== undefined)
-        return (Date.parse(event.endDate) - Date.parse(event.startDate)) / 1000 / 60;
-    }
-
-    function isEqual(event1, event2) {
-      return event1.id === event2.id;
-    }
+    // function getDuration(event) {
+    //   if (event !== undefined)
+    //     return (Date.parse(event.endDate) - Date.parse(event.startDate)) / 1000 / 60;
+    // }
   });
